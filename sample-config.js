97d6c708b3b17c62709d24d8110c0418aed700d1
Yukina Ogasawara <s1f101900723@iniad.org>
2021-12-21 11:10:19 +0900

    Update sample-config.js

--- sample-config.js
+++ sample-config.js
@@ -11,20 +11,19 @@
  * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-var config = {
-    apiKey: "AIzaSyCJVJ8N1QZe6rmnczBDTLe7jI_6mmFGuCk",
-    authDomain: "login-test-b09e3.firebaseapp.com",
-    projectId: "login-test-b09e3",
-    storageBucket: "login-test-b09e3.appspot.com",
-    messagingSenderId: "179784899863",
-    appId: "1:179784899863:web:c2116ddaef6bfc7c89ab52",
-    measurementId: "G-05J8LDGK7P"
-  };
-  firebase.initializeApp(config);
-  import { getAnalytics } from "firebase/analytics";
-
-  const analytics = getAnalytics();
+if(!firebase.app.length) {
+  var config = {
+      apiKey: "AIzaSyCJVJ8N1QZe6rmnczBDTLe7jI_6mmFGuCk",
+      authDomain: "login-test-b09e3.firebaseapp.com",
+      projectId: "login-test-b09e3",
+      storageBucket: "login-test-b09e3.appspot.com",
+      messagingSenderId: "179784899863",
+      appId: "1:179784899863:web:c2116ddaef6bfc7c89ab52",
+      measurementId: "G-05J8LDGK7P"
+    };
+    firebase.initializeApp(config);
+    firebase.analytics();
+}
   
   // Google OAuth Client ID, needed to support One-tap sign-up.
   // Set to null if One-tap sign-up is not supported.
