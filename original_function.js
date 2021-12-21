f2cd5e8081db9a31742942f2034c122532df20ed
Yukina Ogasawara <s1f101900723@iniad.org>
2021-12-21 15:27:31 +0900

    html

--- original_function.js
+++ original_function.js
@@ -112,18 +112,6 @@ function getpretext(num){
 }
 
 function getpreQuestion(num){
-  if(num=='null'){
-    QID = document.getElementsByClassName("Qform-Qid");
-    for (var i = 0; i < QID.length; i++){
-      QID[i].value= null;
-    }
-    Q_title = document.getElementsByClassName("Q_title");
-    for (var i = 0; i < Q_title.length; i++){
-      Q_title[i].value= null;
-    }
-    quillQ.clipboard.dangerouslyPasteHTML();
-  }
-  else{
     var endpoint ="https://script.google.com/macros/s/AKfycbzWQ6ojGAvYrhWerWDPCO11Y_3VGqD_EYcSSyX5ZV7KIanzi-TXOHKsXUHsSBN5mgdiXw/exec";
     fetch(endpoint)
         .then(response => response.json())
@@ -144,5 +132,4 @@ function getpreQuestion(num){
                 Q_title[i].value= pretext_Q[0].title;
               }
         });
-  }
 }
